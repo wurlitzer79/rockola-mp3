1a5cb1fa68c05187ede080ae79246c72a56d28d4
wurlitzer79 <lsastorga@hotmail.com>
2025-01-17 19:37:22 -0300

    nueva versión rockola

---
+++ jss/index.js
@@ -0,0 +1,17 @@
+      const container = document.querySelector(".alphabet-container");
+      const alphabet = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ#";	  
+
+      alphabet.split("").forEach(letter => {
+        const button = document.createElement("button");
+        button.textContent = letter;
+        button.className = "alphabet-button";        
+		//button.innerHTML = `<a href="index.html?valor=${letter}">${letter}</a>`;
+		
+        // Evento para mostrar un alert al hacer clic
+        button.addEventListener("click", () => {
+          //alert(`Has seleccionado la letra: ${letter}`);
+			location.href = `elegido.html?valor=${letter}`;
+        });
+
+        container.appendChild(button);
+      });
---
+++ jss/reproduce.js
@@ -0,0 +1,104 @@
+// Obtener parámetros de la URL
+    const params = new URLSearchParams(window.location.search);
+	const collectionId = params.get("id"); // Obtener el parámetro 'id'  
+	const collectionName = params.get("name"); // Obtener el parámetro 'name'   
+	
+    const apiUrl = `https://archive.org/metadata/${collectionId}`;
+    const songList = document.getElementById("songList");
+    const audioPlayer = document.getElementById("audioPlayer");
+
+    // Mostrar el valor en el título
+	const huno = document.getElementById("titulo");
+	if (huno) {
+      huno.textContent = collectionName;
+    } else {
+      huno.textContent = "Rockola Virtual";
+    }
+
+	//Input oculto para saber el total de canciones cargadas;
+	const tot = document.getElementById("totalsong");
+	tot.value = 0;
+
+    let playlist = []; // Lista de canciones
+    let currentIndex = 0; // Índice de la canción actual
+
+    // Obtener canciones de la API
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const files = data.files.filter(file => file.name.endsWith(".mp3")); // Filtrar solo archivos MP3
+
+        if (files.length > 0) {
+          songList.innerHTML = ""; // Limpiar lista
+
+          files.forEach((file, index) => {
+            const li = document.createElement("li");
+            const button = document.createElement("button");
+			//button.width = "100%";
+            button.textContent = file.title || file.name; // Usar título o nombre del archivo
+			button.id = "btn_" + index;
+            button.onclick = () => playSong(index);
+            li.appendChild(button);
+            songList.appendChild(li);
+
+            // Agregar canción a la playlist
+            playlist.push({
+              name: file.title || file.name,
+              url: `https://archive.org/download/${collectionId}/${file.name}`
+            });
+			tot.value = index + 1;
+          });
+		  
+        } else {
+          songList.innerHTML = "<li>No se encontraron canciones.</li>";
+        }
+      })
+      .catch(error => {
+        console.error("Error al cargar canciones:", error);
+        songList.innerHTML = "<li>Error al cargar las canciones.</li>";
+      });
+	  
+	// Desactiva todos los botones
+	function desactivabtns(){
+		// Crear un array con los índices del 0 al 21
+		const indices = document.getElementById("totalsong").value;
+		for (let i = 0; i <= indices - 1; i++) {
+		  document.getElementById("btn_" + i).classList.remove("active");
+		}
+	}
+	
+    // Reproducir canción por índice
+    function playSong(index) {	  
+      currentIndex = index;
+      audioPlayer.src = playlist[currentIndex].url;
+	  songName.textContent = `Reproduciendo: ` + playlist[currentIndex].name;	  
+      audioPlayer.play();
+	  
+	  //desactivabtns();	
+	  const indices = document.getElementById("totalsong").value;
+	  for (let i = 0; i <= indices - 1; i++) {
+	    document.getElementById("btn_" + i).classList.remove("active");
+	  }	  
+	  //alert(indices);
+	  document.getElementById("btn_" + index).classList.add("active");
+    }
+
+    // Reproducir siguiente canción al finalizar
+    audioPlayer.addEventListener("ended", () => {
+      currentIndex = (currentIndex + 1) % playlist.length; // Avanzar y reiniciar al final
+      playSong(currentIndex);
+    });
+	
+	// Limpiar el nombre de la canción cuando se pausa la reproducción
+	audioPlayer.addEventListener("pause", () => {
+	  if (audioPlayer.currentTime !== audioPlayer.duration) {
+		songName.textContent = `Reproduciendo: `; // Limpia el texto del nombre
+	  }
+	});
+	
+	// Evento Play
+	audioPlayer.addEventListener("play", () => {
+	  if (audioPlayer.currentTime !== 0) { // Verifica que no sea el inicio del audio
+		songName.textContent = `Reproduciendo: ` + playlist[currentIndex].name;
+	  }
+	});	
